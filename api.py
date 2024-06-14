08213824f82d3bf7f850c11aab2907d6f3bcfb03
AxeUnder <yachmenyov.alex@yandex.ru>
2024-06-14 14:44:38 +0700

    TImproved code readability beta v0.5.5

--- api.py
+++ api.py
@@ -2,7 +2,6 @@ import aiohttp
 import logging
 from config import *
 
-
 # Настройка логирования
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
